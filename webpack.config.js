e121446f13531599d7fe296642a2d44cd4ffcad1
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-04-05 12:46:35 -0400

    webpack config and babel

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+
+let mode = 'development'
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundler.js'
+  },
+  mode: mode,
+  output: {
+    path: path.join(__dirname, '/dist'),
+  },
+  devServer: {
+    watchContentBase: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(css|sass|scss)$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/i,
+        use: 'url-loader',
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+  ],
+}
