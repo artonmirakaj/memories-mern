3fade1274ecd2019031a1369828c96c984d00107
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-04-06 12:23:33 -0400

    mongo db connection and setting up router

--- server/index.js
+++ server/index.js
@@ -1,5 +1,30 @@
-import express from 'express';
-import cors from 'cors'
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+require('dotenv').config();
+
+const { mongoConnect } = require('./src/services/mongo');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(cors({
+  origin: 'htpp://localhost:3000'
+}));
+app.use(morgan('combined'));
+app.use(express.json());
+
+const server = http.createServer(app);
+
+async function startServer() {
+  await mongoConnect();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}...`);
+  });
+}
+
+startServer();
