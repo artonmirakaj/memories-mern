03de4d3e30a3accede8d3808019bec3dd8a51c31
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-04-10 10:53:26 -0400

    update and delete functionality

--- server/src/controllers/posts.controller.js
+++ server/src/controllers/posts.controller.js
@@ -33,13 +33,26 @@ const updatePost = async (req, res) => {
     return res.status(404).send('No Post with that ID!')
   }
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
 
   res.json(updatedPost);
+}
+
+const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send('No Post with that ID!')
+  }
+
+  await PostMessage.findByIdAndRemove(id)
+
+  res.json({ message: 'Post Deleted!' })
 }
 
 module.exports = {
   getPosts,
   createPost,
   updatePost,
+  deletePost,
 }
--- server/src/routes/posts/posts.router.js
+++ server/src/routes/posts/posts.router.js
@@ -3,6 +3,7 @@ const {
   getPosts,
   createPost,
   updatePost,
+  deletePost,
 } = require('../../controllers/posts.controller');
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPost);
 router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 module.exports = router;
