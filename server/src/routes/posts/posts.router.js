39161080187462b1e9c7e416c90fe7ae9c6431bd
Arton Mirakaj <artonmirakaj@Arton.lan>
2022-04-13 12:33:35 -0400

    like post

--- server/src/routes/posts/posts.router.js
+++ server/src/routes/posts/posts.router.js
@@ -4,6 +4,7 @@ const {
   createPost,
   updatePost,
   deletePost,
+  likePost,
 } = require('../../controllers/posts.controller');
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.put('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.put('/:id/likePost', likePost);
 
 module.exports = router;
